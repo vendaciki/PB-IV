a7553e0cb110bce92cf605378fdfbcfbc54fcad7
Vendula Martinková <vendaciki@seznam.cz>
2024-08-04 08:22:31 +0200

    new

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// Get Current Year
+function getCurrentYear() {
+    var d = new Date();
+    var year = d.getFullYear();
+    document.querySelector("#displayDateYear").innerText = year;
+}
+getCurrentYear()
